0a7ba3ff6d3466035a10171ed5f4e3afe2945c7e
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-03-04 12:23:57 +0900

    :art: Add DashBoard and create styles
    t

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,74 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const Dashboard = () => {
-  return (<div>Dashboard</div>)
+  return (
+    <Fragment>
+      <div className='header'>
+        <section className="budget">
+          <div className="budget_inner">
+              <p className="budget_title">Total Balance</p>
+              <span className="budget_value">5000円</span>
+            <div className='budget_box'>
+              <div className='budget_items'>
+                <p className="budget_title">Income</p>
+                <span className="budget_value-sub">5000円</span>
+              </div>
+              <div className='budget_items'>
+                <p className="budget_title">Expense</p>
+                <span className="budget_value-sub">5000円</span>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <main>
+        <section className='transaction'>
+          <div className='transaction_header'>
+            <div className='transaction_sort'>recent▼</div>
+            <div className='transaction_sort'>amount▼</div>
+          </div>
+          <ul className='transaction_lists'>
+            <li className='transaction_list'>
+              <div>
+                <h3>Travel</h3>
+                <p>In 8 hours</p>
+              </div>
+              <div className="positive">
+                + 5000 円
+              </div>
+            </li>
+            <li className='transaction_list'>
+              <div>
+                <h3>Food</h3>
+                <p>In 8 hours</p>
+              </div>
+              <div className="negative">
+                - 5000 円
+              </div>
+            </li>
+            <li className='transaction_list'>
+              <div>
+                <h3>Travel</h3>
+                <p>In 8 hours</p>
+              </div>
+              <div className="positive">
+                + 5000 円
+              </div>
+            </li>
+             <li className='transaction_list'>
+              <div>
+                <h3>Food</h3>
+                <p>In 8 hours</p>
+              </div>
+              <div className="negative">
+                - 5000 円
+              </div>
+            </li>
+          </ul>
+        </section>
+      </main>
+    </Fragment>
+  )
 }
 
 export default Dashboard
