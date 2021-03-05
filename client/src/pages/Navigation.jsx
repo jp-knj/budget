9458fb957fbfab22bb599a3d6bfa0550e052bd5d
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-03-05 11:37:24 +0900

    :sparkles: Add Navagation page to move other page when clicked
    t

--- client/src/pages/Navigation.jsx
+++ client/src/pages/Navigation.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+
+import { NavLink, withRouter } from 'react-router-dom';
 
 // Material-UI Icon
 import { Add } from '@material-ui/icons'
@@ -8,29 +10,46 @@ import { MenuItem, MenuList, ListItemIcon } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 // Component
-import TransactionForm from '../components/TransactionForm'
+// import TransactionForm from '../components/TransactionForm'
 
 const Navigation = ({ children }) => {
-  const useStyles = makeStyles((theme) => ({
+  const useStyles = makeStyles(() => ({
     addCircleIcon: {
       color: 'white'
     }
   }))
   const classes = useStyles();
   return (
-    <div>
+    <Fragment>
+    <div className="sidebar">
+      <h3>Friday, 5 March</h3>
+      <h4>Good Morning</h4>
+      <h1>Kenji Tomita</h1>
+
       <nav className="navigation">
         <MenuList>
-          <MenuItem>
+          <MenuItem
+            to="/dashboard"
+            component={NavLink}
+          >
             <ListItemIcon>DashBoard</ListItemIcon>
           </MenuItem>
-          <MenuItem>
+          <MenuItem
+            to="/transaction"
+            component={NavLink}
+          >
             <ListItemIcon>Transactions</ListItemIcon>
           </MenuItem>
-          <MenuItem>
+          <MenuItem
+            to="/statistics"
+            component={NavLink}
+          >
             <ListItemIcon>Statistics</ListItemIcon>
           </MenuItem>
-          <MenuItem>
+          <MenuItem
+            to="/profile"
+            component={NavLink}
+          >
             <ListItemIcon>Profile</ListItemIcon>
           </MenuItem>
           <MenuItem>
@@ -38,12 +57,12 @@ const Navigation = ({ children }) => {
           </MenuItem>
         </MenuList>
       </nav>
+      </div>
       <main>{children}</main>
       <div className="btn">
         <Add className={`${classes.addCircleIcon}`} />
       </div>
-      <TransactionForm />
-    </div>
+    </Fragment>
   )
 }
 export default Navigation
